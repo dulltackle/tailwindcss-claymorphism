55b70c8d9e18f1e32f3b55a03df26190d23349d9
dulltackle <dulltackle@outlook.com>
2022-07-20 10:55:40 +0800

    test: :test_tube: add failing test for `cssMatcher()`

---
+++ src/util.test.ts
@@ -0,0 +1,41 @@
+import { describe, test } from "vitest"
+
+describe("`cssMatcher()`", () => {
+  test("two css classes with same content should be equal(return `equalObj`)", ({
+    expect,
+  }) => {
+    const received = ".clay-md-red: { backgroundColor: #f87171; }"
+    const expected = `
+      .clay-md-red: {
+        backgroundColor: #f87171;
+      }
+    `
+    const equalObj = {
+      message: () => `expected ${received} not to match CSS ${expected}`,
+      pass: true,
+    }
+
+    const matcher = cssMatcher(received, expected)
+    expect(matcher.pass).toBe(equalObj.pass)
+    expect(matcher.message()).toBe(equalObj.message())
+  })
+
+  test("two css classes with different content should not be equal(return `notEqualObj`)", ({
+    expect,
+  }) => {
+    const received = ".text-color: { color: #fff; }"
+    const expected = `
+      .clay-md-red: {
+        backgroundColor: #f87171;
+      }
+    `
+    const notEqualObj = {
+      message: () => `expected ${received} to match CSS ${expected}`,
+      pass: false,
+    }
+
+    const matcher = cssMatcher(received, expected)
+    expect(matcher.pass).toBe(notEqualObj.pass)
+    expect(matcher.message()).toBe(notEqualObj.message())
+  })
+})
