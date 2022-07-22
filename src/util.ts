8e0d544bc0ec0a5927a02ea5c11b1deffe6c28a3
dulltackle <dulltackle@outlook.com>
2022-07-22 15:52:06 +0800

    feat: :technologist: `cssMatcher()` throws error if the type of received value or expected value is not `string`

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,26 @@
+import {
+  matcherErrorMessage,
+  matcherHint,
+  printExpected,
+  printReceived,
+  printWithType,
+} from "jest-matcher-utils"
+
 export const cssMatcher = (received: string, expected: string) => {
+  for (const element of [received, expected]) {
+    if (typeof element !== "string") {
+      throw new Error(
+        matcherErrorMessage(
+          matcherHint("toMatchCss", String(received), String(expected)),
+          `value must be a string`,
+          printWithType("Expected", expected, printExpected) +
+            "\n" +
+            printWithType("Received", received, printReceived)
+        )
+      )
+    }
+  }
+
   const whitespaceRemoved = (str: string) => str.replace(/[;\s]/g, "")
 
   if (whitespaceRemoved(received) === whitespaceRemoved(expected)) {
