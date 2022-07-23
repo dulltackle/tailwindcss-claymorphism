1be8c8d07ad6fc6491ba32bfa48ea12fa848a40e
dulltackle <dulltackle@outlook.com>
2022-07-23 15:37:38 +0800

    refactor: :recycle: rafactor the tests for `cssMatcher()`

--- src/util.test.ts
+++ src/util.test.ts
@@ -2,59 +2,34 @@ import { describe, test } from "vitest"
 import { cssMatcher } from "./util"
 
 describe("`cssMatcher()`", () => {
-  test("two css classes with same content should be equal(return `equalObj`)", ({
-    expect,
-  }) => {
-    const received = ".clay-md-red: { backgroundColor: #f87171; }"
+  test("two css classes with same content should be equal", ({ expect }) => {
+    const received = ".clay-md-red { backgroundColor: #f87171; }"
     const expected = `
-      .clay-md-red: {
+      .clay-md-red {
         backgroundColor: #f87171;
       }
     `
-    const equalObj = {
-      message: () => `expected ${received} not to match CSS ${expected}`,
-      pass: true,
-    }
 
     const matcher = cssMatcher(received, expected)
-    expect(matcher.pass).toBe(equalObj.pass)
-    expect(matcher.message()).toBe(equalObj.message())
+    expect(matcher.pass).toBeTruthy()
   })
 
-  test("two css classes with different content should not be equal(return `notEqualObj`)", ({
-    expect,
-  }) => {
-    const received = ".text-color: { color: #fff; }"
+  test("two css classes with different content should not be equal", ({ expect }) => {
+    const received = ".text-color { color: #fff; }"
     const expected = `
-      .clay-md-red: {
+      .clay-md-red {
         backgroundColor: #f87171;
       }
     `
-    const notEqualObj = {
-      message: () => `expected ${received} to match CSS ${expected}`,
-      pass: false,
-    }
 
     const matcher = cssMatcher(received, expected)
-    expect(matcher.pass).toBe(notEqualObj.pass)
-    expect(matcher.message()).toBe(notEqualObj.message())
+    expect(matcher.pass).toBeFalsy()
   })
 
-  test("should throw a error when `received` is not string", ({ expect }) => {
-    // use `never` to placate TS only
-    const received = Math.floor(Math.random() * 1000) as never
-    const expected = `
-      .clay-md-red: {
-        backgroundColor: #f87171;
-      }
-    `
-    expect(() => cssMatcher(received, expected)).toThrowError()
-  })
-
-  test("should throw a error when `expected` is not string", ({ expect }) => {
-    const received = ".text-color: { color: #fff; }"
-    // use `never` to placate TS only
-    const expected = Math.floor(Math.random() * 1000) as never
+  test("should throw a error when `received` or `expected` is not string", ({ expect }) => {
+    const random = Math.floor(Math.random() * 1000)
+    const [received, expected] = random < 500 ? [random, random.toString()] : [random.toString(), random]
+    // @ts-expect-error: test the throwing error feature
     expect(() => cssMatcher(received, expected)).toThrowError()
   })
 })
