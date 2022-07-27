712b1126e6e1d68e4baceb685d8c1cc427841e0b
dulltackle <dulltackle@outlook.com>
2022-07-27 16:03:19 +0800

    build: :technologist: config path alias `~` resolving `src`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
+import { resolve } from "path"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+  resolve: {
+    alias: { "~": resolve(__dirname, "src") },
+  },
   test: {
     restoreMocks: true,
   },
