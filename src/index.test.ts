128a0840de7ddd8fb7994f646e9ade9d70702f73
dulltackle <dulltackle@outlook.com>
2022-07-27 15:21:39 +0800

    refactor: :recycle: refactor tests for `generateClayCss()` and `generateAllClayCss()`

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,44 +9,42 @@ beforeAll(() => {
   expect.extend({ toMatchCss: cssMatcher })
 })
 
-describe.each([
-  {
-    colors: [
-      {
-        name: "red",
-        background: "#f87171",
-        insetShadowPrimary: "#ef4444",
-        insetShadowSecondary: "#fca5a5",
-      },
-      {
-        name: "orange",
-        background: "#fb923c",
-        insetShadowPrimary: "#f97316",
-        insetShadowSecondary: "#fdba74",
-      },
-    ],
-    shadows: [
-      {
-        name: "medium",
-        acronym: "md",
-        outset: "8px 8px 16px rgba(0, 0, 0, .25)",
-        insetPrimary: "inset -8px -8px 32px",
-        insetSecondary: "inset 8px 8px 16px",
-        insetModifier: "inset -2px -2px 4px #fafafa",
-      },
-      {
-        name: "small",
-        acronym: "sm",
-        outset: "4px 4px 8px rgba(0, 0, 0, .25)",
-        insetPrimary: "inset -4px -4px 16px",
-        insetSecondary: "inset 4px 4px 8px",
-        insetModifier: "inset -1px -1px 2px #fafafa",
-      },
-    ],
-  },
-])("generate clay css", ({ colors, shadows }) => {
+describe("generate clay css", () => {
+  const colors = [
+    {
+      name: "red",
+      background: "#f87171",
+      insetShadowPrimary: "#ef4444",
+      insetShadowSecondary: "#fca5a5",
+    },
+    {
+      name: "orange",
+      background: "#fb923c",
+      insetShadowPrimary: "#f97316",
+      insetShadowSecondary: "#fdba74",
+    },
+  ]
+  const shadows = [
+    {
+      name: "medium",
+      acronym: "md",
+      outset: "8px 8px 16px rgba(0, 0, 0, .25)",
+      insetPrimary: "inset -8px -8px 32px",
+      insetSecondary: "inset 8px 8px 16px",
+      insetModifier: "inset -2px -2px 4px #fafafa",
+    },
+    {
+      name: "small",
+      acronym: "sm",
+      outset: "4px 4px 8px rgba(0, 0, 0, .25)",
+      insetPrimary: "inset -4px -4px 16px",
+      insetSecondary: "inset 4px 4px 8px",
+      insetModifier: "inset -1px -1px 2px #fafafa",
+    },
+  ]
   test("generate a clay css with a color and a shadow", ({ expect }) => {
-    const red = colors[0]
+    const [red, orange] = colors
+    const [md, sm] = shadows
     const mediumRed = {
       [".clay-md-red"]: {
         backgroundColor: "#f87171",
@@ -59,8 +57,22 @@ describe.each([
         boxShadow: "4px 4px 8px rgba(0, 0, 0, .25),inset -4px -4px 16px #ef4444,inset 4px 4px 8px #fca5a5,inset -1px -1px 2px #fafafa",
       },
     }
-    expect(generateClayCss(red, shadows[0])).toEqual(mediumRed)
-    expect(generateClayCss(red, shadows[1])).toEqual(smallRed)
+    const mediumOrange = {
+      [".clay-md-orange"]: {
+        backgroundColor: "#fb923c",
+        boxShadow: "8px 8px 16px rgba(0, 0, 0, .25),inset -8px -8px 32px #f97316,inset 8px 8px 16px #fdba74,inset -2px -2px 4px #fafafa",
+      },
+    }
+    const smallOrange = {
+      [".clay-sm-orange"]: {
+        backgroundColor: "#fb923c",
+        boxShadow: "4px 4px 8px rgba(0, 0, 0, .25),inset -4px -4px 16px #f97316,inset 4px 4px 8px #fdba74,inset -1px -1px 2px #fafafa",
+      },
+    }
+    expect(generateClayCss(red, md)).toEqual(mediumRed)
+    expect(generateClayCss(red, sm)).toEqual(smallRed)
+    expect(generateClayCss(orange, md)).toEqual(mediumOrange)
+    expect(generateClayCss(orange, sm)).toEqual(smallOrange)
   })
   test("generate clay css with colors and shadows", ({ expect }) => {
     const aimedClayCss = [
