aaffc3fa582269a30ba9378cafc3e022d47d1a9a
dulltackle <dulltackle@outlook.com>
2022-07-27 16:19:45 +0800

    build: :wrench: add vite config for build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import { resolve } from "path"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      formats: ["cjs"],
+      name: "TailwindcssClay",
+      fileName: "index",
+    },
+    outDir: "lib",
+  },
   resolve: {
     alias: { "~": resolve(__dirname, "src") },
   },
