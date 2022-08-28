ee44ed762aa4eba7bb76d5648dac5f489fb33d97
dulltackle <45963660+dulltackle@users.noreply.github.com>
2022-08-28 17:11:06 +0800

    feat: :boom: color value only accepts HEX (#17)
    * test: :test_tube: add a failing test for "value of color accepts HEX only"
    
    * feat: :boom: color value only accepts HEX

--- test/util.test.ts
+++ test/util.test.ts
@@ -125,6 +125,38 @@ describe.each([
       ],
     },
   },
+  {
+    aimedResult: false,
+    input: {
+      colors: [
+        {
+          // value of color accepts HEX only
+          name: "red",
+          background: "rgba(27,31,36,0.15)",
+          insetShadowPrimary: "hwb(213deg 14% 82%)",
+          insetShadowSecondary: "hsl(213deg 13% 16%)",
+        },
+      ],
+      shadows: [
+        {
+          name: "medium",
+          acronym: "md",
+          outset: "8px 8px 16px rgba(0, 0, 0, .25)",
+          insetPrimary: "inset -8px -8px 32px",
+          insetSecondary: "inset 8px 8px 16px",
+          insetModifier: "inset -2px -2px 4px #fafafa",
+        },
+        {
+          name: "small",
+          acronym: "sm",
+          outset: "4px 4px 8px rgba(0, 0, 0, .25)",
+          insetPrimary: "inset -4px -4px 16px",
+          insetSecondary: "inset 4px 4px 8px",
+          insetModifier: "inset -1px -1px 2px #fafafa",
+        },
+      ],
+    },
+  },
 ])("isClay()", ({ input, aimedResult }) => {
   test("isClay() should judge the type of input is `Clay` or not", ({ expect }) => {
     expect(isClay(input)).toBe(aimedResult)
