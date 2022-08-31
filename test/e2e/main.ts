6d97b2b8ef2bcbd9e303c8a619750291c6b9a670
dulltackle <dulltackle@outlook.com>
2022-08-31 16:40:34 +0800

    test: :wrench: set up plugin tailwindcss-claymorphism

--- test/e2e/main.ts
+++ test/e2e/main.ts
@@ -1,5 +1,5 @@
 import "./index.css"
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="text-red-500">Hello tailwindcss-claymorphism</div>
+  <div class="clay-md-red">Hello tailwindcss-claymorphism</div>
 `
