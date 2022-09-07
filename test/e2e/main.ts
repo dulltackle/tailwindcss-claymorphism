cd4d88e60d33a2989743993ca9d219f9c3d5a990
dulltackle <dulltackle@outlook.com>
2022-09-07 17:02:20 +0800

    feat: :lipstick: set layout for `Card`

--- test/e2e/main.ts
+++ test/e2e/main.ts
@@ -1,5 +1,8 @@
 import "./index.css"
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="clay-md-red">Hello tailwindcss-claymorphism</div>
+  <div class="flex flex-wrap">
+    <div class="basis-1/4 aspect-video p-2">
+    </div>
+  </div>
 `
