352118fc36824e627f9e7ca22bf2246addf3a3ca
dulltackle <dulltackle@outlook.com>
2022-09-07 17:06:46 +0800

    feat: :sparkles: add component `Card`

---
+++ test/e2e/card/index.ts
@@ -0,0 +1,1 @@
+export const Card = (): string => `<p class="h-full clay-md-red">Hello tailwindcss-claymorphism</p>`
--- test/e2e/main.ts
+++ test/e2e/main.ts
@@ -1,8 +1,10 @@
+import { Card } from "./card"
 import "./index.css"
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="flex flex-wrap">
     <div class="basis-1/4 aspect-video p-2">
+      ${Card()}
     </div>
   </div>
 `
