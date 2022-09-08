e2d6bc4c2aae3e9120277ebfa98f37354f094c35
dulltackle <dulltackle@outlook.com>
2022-09-08 17:11:53 +0800

    feat: :lipstick: finish layout

--- test/e2e/main.ts
+++ test/e2e/main.ts
@@ -1,11 +1,13 @@
 import "./index.css"
 
 const Card = (): string => `<li class="basis-1/4 aspect-video p-2">
-  <p class="h-full clay-md-red">Hello tailwindcss-claymorphism</p>
+  <div class="h-full flex flex-col justify-center items-center clay-md-red rounded-xl">
+    <p class="text-lg text-white">Hello tailwindcss-claymorphism</p>
+  </div>
 </li>`
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <ul class="flex flex-wrap">
-      ${Card()}
+    ${Card().repeat(6)}
   </ul>
 `
