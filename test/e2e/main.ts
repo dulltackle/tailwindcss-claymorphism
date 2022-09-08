91883ffa508d27f199bbc5189cbda5967bb619e7
dulltackle <dulltackle@outlook.com>
2022-09-08 16:54:32 +0800

    refactor: :recycle: replace `<div>` with `<ul>` and `<li>` for semantic reason

--- test/e2e/main.ts
+++ test/e2e/main.ts
@@ -1,13 +1,11 @@
 import "./index.css"
 
-const Card = (): string => `<p class="h-full clay-md-red">
-  Hello tailwindcss-claymorphism
-</p>`
+const Card = (): string => `<li class="basis-1/4 aspect-video p-2">
+  <p class="h-full clay-md-red">Hello tailwindcss-claymorphism</p>
+</li>`
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="flex flex-wrap">
-    <div class="basis-1/4 aspect-video p-2">
+  <ul class="flex flex-wrap">
       ${Card()}
-    </div>
-  </div>
+  </ul>
 `
