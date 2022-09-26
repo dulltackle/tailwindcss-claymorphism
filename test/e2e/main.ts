0a51fc46e8b1fe177aab2de057c44f0403d2430c
dulltackle <dulltackle@outlook.com>
2022-09-26 17:08:43 +0800

    feat: :sparkles: set up data structure for e2e test

--- test/e2e/main.ts
+++ test/e2e/main.ts
@@ -1,13 +1,18 @@
 import "./index.css"
 
-const Card = (): string => `<li class="basis-1/4 aspect-video p-2">
-  <div class="h-full flex flex-col justify-center items-center clay-md-red rounded-xl">
+const CLAY_CLASS_NAMES: [string, string][] = [["clay-sm-red", "clay-md-red"]]
+
+const Card = (clayClassName: string): string => `<li class="basis-1/4 aspect-video p-2">
+  <div class="h-full flex flex-col justify-center items-center ${clayClassName} rounded-xl">
     <p class="text-lg text-white">Hello tailwindcss-claymorphism</p>
   </div>
 </li>`
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <ul class="flex flex-wrap">
-    ${Card().repeat(6)}
+    ${CLAY_CLASS_NAMES.map((color) => color.map((clayClassName) => Card(clayClassName))).reduce(
+      (previousValue, currentValue) => `${previousValue} ${currentValue.join(" ")}`,
+      ""
+    )}
   </ul>
 `
