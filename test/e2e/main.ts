67d1335a7e0c63ac45fbca89119fe42ce8d7c641
dulltackle <dulltackle@outlook.com>
2022-09-27 15:48:16 +0800

    refactor: :recycle: decouple `<li>` from `Card` and `Button`

--- test/e2e/main.ts
+++ test/e2e/main.ts
@@ -4,11 +4,11 @@ const COLUMN_AMOUNT = 4
 
 const CLAY_CLASS_NAMES: [string, string][] = [["clay-sm-red", "clay-md-red"]]
 
-const Card = (clayClassName: string): string => `<li class="aspect-video p-2">
+const Card = (clayClassName: string): string => `
   <div class="h-full flex flex-col justify-center items-center ${clayClassName} rounded-xl">
     <p class="text-lg text-white text-center">Hello tailwindcss-claymorphism</p>
   </div>
-</li>`
+`
 
 const Button = (clayClassName: string): string => "hi"
 
@@ -16,7 +16,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <ul class="grid" style="grid-template-columns: repeat(${COLUMN_AMOUNT}, 1fr)">
     ${CLAY_CLASS_NAMES.map((clayColor) => {
       const [clayColorSmall, clayColorMiddle] = clayColor
-      return [Card(clayColorMiddle), Button(clayColorSmall)].join(" ")
+      return [`<li class="aspect-video p-2">${Card(clayColorMiddle)}</li>`, `<li class="aspect-video p-2">${Button(clayColorSmall)}</li>`].join(" ")
     }).join(" ")}
   </ul>
 `
