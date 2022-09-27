0e937af97a0dfe7d33cd55ec684574dd7881d35f
dulltackle <dulltackle@outlook.com>
2022-09-27 15:22:18 +0800

    refactor: :lipstick: set layout with `grid`

--- test/e2e/main.ts
+++ test/e2e/main.ts
@@ -1,15 +1,17 @@
 import "./index.css"
 
+const COLUMN_AMOUNT = 4
+
 const CLAY_CLASS_NAMES: [string, string][] = [["clay-sm-red", "clay-md-red"]]
 
-const Card = (clayClassName: string): string => `<li class="basis-1/4 aspect-video p-2">
+const Card = (clayClassName: string): string => `<li class="aspect-video p-2">
   <div class="h-full flex flex-col justify-center items-center ${clayClassName} rounded-xl">
-    <p class="text-lg text-white">Hello tailwindcss-claymorphism</p>
+    <p class="text-lg text-white text-center">Hello tailwindcss-claymorphism</p>
   </div>
 </li>`
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <ul class="flex flex-wrap">
+  <ul class="grid" style="grid-template-columns: repeat(${COLUMN_AMOUNT}, 1fr)">
     ${CLAY_CLASS_NAMES.map((color) => color.map((clayClassName) => Card(clayClassName))).reduce(
       (previousValue, currentValue) => `${previousValue} ${currentValue.join(" ")}`,
       ""
