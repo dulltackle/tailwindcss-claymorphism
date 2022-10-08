8e184d0884c874dc4b53949b50fa6b757d411342
dulltackle <dulltackle@outlook.com>
2022-10-08 17:23:25 +0800

    test: :recycle: refactor test for `generateAllClayCss()`

--- test/main.test.ts
+++ test/main.test.ts
@@ -33,66 +33,65 @@ describe("test helper functions for generating clay utility class ", () => {
     })
   })
 
-  const colors = [
-    {
-      name: "red",
-      background: "#f87171",
-      insetShadowPrimary: "#ef4444",
-      insetShadowSecondary: "#fca5a5",
-    },
-    {
-      name: "orange",
-      background: "#fb923c",
-      insetShadowPrimary: "#f97316",
-      insetShadowSecondary: "#fdba74",
-    },
-  ]
-  const shadows = [
-    {
-      name: "medium",
-      acronym: "md",
-      outset: "8px 8px 16px rgba(0, 0, 0, .25)",
-      insetPrimary: "inset -8px -8px 32px",
-      insetSecondary: "inset 8px 8px 16px",
-      insetModifier: "inset -2px -2px 4px #fafafa",
-    },
-    {
-      name: "small",
-      acronym: "sm",
-      outset: "4px 4px 8px rgba(0, 0, 0, .25)",
-      insetPrimary: "inset -4px -4px 16px",
-      insetSecondary: "inset 4px 4px 8px",
-      insetModifier: "inset -1px -1px 2px #fafafa",
-    },
-  ]
-  test("generate clay css with colors and shadows", ({ expect }) => {
-    const aimedClayCss = [
+  test("generateAllClayCss() for generating clay utility classes with colors and shadows", ({ expect }) => {
+    const mockedColors: Color[] = [
       {
-        [".clay-md-red"]: {
-          backgroundColor: "#f87171",
-          boxShadow: "8px 8px 16px rgba(0, 0, 0, .25),inset -8px -8px 32px #ef4444,inset 8px 8px 16px #fca5a5,inset -2px -2px 4px #fafafa",
-        },
+        name: "navajo",
+        background: "#e2c599",
+        insetShadowPrimary: "#af967f",
+        insetShadowSecondary: "#feeed4",
       },
       {
-        [".clay-sm-red"]: {
-          backgroundColor: "#f87171",
-          boxShadow: "4px 4px 8px rgba(0, 0, 0, .25),inset -4px -4px 16px #ef4444,inset 4px 4px 8px #fca5a5,inset -1px -1px 2px #fafafa",
-        },
-      },
-      {
-        [".clay-md-orange"]: {
-          backgroundColor: "#fb923c",
-          boxShadow: "8px 8px 16px rgba(0, 0, 0, .25),inset -8px -8px 32px #f97316,inset 8px 8px 16px #fdba74,inset -2px -2px 4px #fafafa",
-        },
-      },
-      {
-        [".clay-sm-orange"]: {
-          backgroundColor: "#fb923c",
-          boxShadow: "4px 4px 8px rgba(0, 0, 0, .25),inset -4px -4px 16px #f97316,inset 4px 4px 8px #fdba74,inset -1px -1px 2px #fafafa",
-        },
+        name: "turquoise",
+        background: "#48d1cc",
+        insetShadowPrimary: "#66cdaa",
+        insetShadowSecondary: "#40e0d0",
       },
     ]
-    expect(generateAllClayCss({ colors, shadows })).toEqual(aimedClayCss)
+    const mockedShadows: Shadow[] = [
+      {
+        name: "large",
+        acronym: "lg",
+        outset: "12px 12px 24px rgba(0, 0, 0, .25)",
+        insetPrimary: "inset -12px -12px 48px",
+        insetSecondary: "inset 12px 12px 24px",
+        insetModifier: "inset -3px -3px 6px #fafafa",
+      },
+      {
+        name: "extra large",
+        acronym: "xl",
+        outset: "16px 16px 32px rgba(0, 0, 0, .25)",
+        insetPrimary: "inset -16px -16px 64px",
+        insetSecondary: "inset 16px 16px 32px",
+        insetModifier: "inset -4px -4px 8px #fafafa",
+      },
+    ]
+    expect(generateAllClayCss({ colors: mockedColors, shadows: mockedShadows })).toEqual([
+      {
+        [".clay-lg-navajo"]: {
+          backgroundColor: "#e2c599",
+          boxShadow: "12px 12px 24px rgba(0, 0, 0, .25),inset -12px -12px 48px #af967f,inset 12px 12px 24px #feeed4,inset -3px -3px 6px #fafafa",
+        },
+      },
+      {
+        [".clay-xl-navajo"]: {
+          backgroundColor: "#e2c599",
+          boxShadow: "16px 16px 32px rgba(0, 0, 0, .25),inset -16px -16px 64px #af967f,inset 16px 16px 32px #feeed4,inset -4px -4px 8px #fafafa",
+        },
+      },
+      {
+        [".clay-lg-turquoise"]: {
+          backgroundColor: "#48d1cc",
+          boxShadow: "12px 12px 24px rgba(0, 0, 0, .25),inset -12px -12px 48px #66cdaa,inset 12px 12px 24px #40e0d0,inset -3px -3px 6px #fafafa",
+        },
+      },
+      {
+        [".clay-xl-turquoise"]: {
+          backgroundColor: "#48d1cc",
+          boxShadow: "16px 16px 32px rgba(0, 0, 0, .25),inset -16px -16px 64px #66cdaa,inset 16px 16px 32px #40e0d0,inset -4px -4px 8px #fafafa",
+        },
+      },
+    ])
   })
 })
 
