d9c58fa253bbbbf5acb7c113ababdd277b514e01
dulltackle <dulltackle@outlook.com>
2022-10-20 15:55:55 +0800

    refactor: :recycle: refactor the way to generate `defaultClayColors`

--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,112 +1,16 @@
-export const defaultClayColors: Record<string, ClayColor> = {
-  slate: {
-    background: "#94a3b8",
-    insetShadowPrimary: "#64748b",
-    insetShadowSecondary: "#e2e8f0",
-  },
-  gray: {
-    background: "#9ca3af",
-    insetShadowPrimary: "#6b7280",
-    insetShadowSecondary: "#e5e7eb",
-  },
-  zinc: {
-    background: "#a1a1aa",
-    insetShadowPrimary: "#71717a",
-    insetShadowSecondary: "#e4e4e7",
-  },
-  neutral: {
-    background: "#a3a3a3",
-    insetShadowPrimary: "#737373",
-    insetShadowSecondary: "#e5e5e5",
-  },
-  stone: {
-    background: "#a8a29e",
-    insetShadowPrimary: "#78716c",
-    insetShadowSecondary: "#e7e5e4",
-  },
-  red: {
-    background: "#f87171",
-    insetShadowPrimary: "#ef4444",
-    insetShadowSecondary: "#fecaca",
-  },
-  orange: {
-    background: "#fb923c",
-    insetShadowPrimary: "#f97316",
-    insetShadowSecondary: "#fed7aa",
-  },
-  amber: {
-    background: "#fbbf24",
-    insetShadowPrimary: "#f59e0b",
-    insetShadowSecondary: "#fde68a",
-  },
-  yellow: {
-    background: "#facc15",
-    insetShadowPrimary: "#eab308",
-    insetShadowSecondary: "#fef08a",
-  },
-  lime: {
-    background: "#a3e635",
-    insetShadowPrimary: "#84cc16",
-    insetShadowSecondary: "#d9f99d",
-  },
-  green: {
-    background: "#4ade80",
-    insetShadowPrimary: "#22c55e",
-    insetShadowSecondary: "#bbf7d0",
-  },
-  emerald: {
-    background: "#34d399",
-    insetShadowPrimary: "#10b981",
-    insetShadowSecondary: "#a7f3d0",
-  },
-  teal: {
-    background: "#2dd4bf",
-    insetShadowPrimary: "#14b8a6",
-    insetShadowSecondary: "#99f6e4",
-  },
-  cyan: {
-    background: "#22d3ee",
-    insetShadowPrimary: "#06b6d4",
-    insetShadowSecondary: "#a5f3fc",
-  },
-  sky: {
-    background: "#38bdf8",
-    insetShadowPrimary: "#0ea5e9",
-    insetShadowSecondary: "#bae6fd",
-  },
-  blue: {
-    background: "#60a5fa",
-    insetShadowPrimary: "#3b82f6",
-    insetShadowSecondary: "#bfdbfe",
-  },
-  indigo: {
-    background: "#818cf8",
-    insetShadowPrimary: "#6366f1",
-    insetShadowSecondary: "#c7d2fe",
-  },
-  violet: {
-    background: "#a78bfa",
-    insetShadowPrimary: "#8b5cf6",
-    insetShadowSecondary: "#ddd6fe",
-  },
-  purple: {
-    background: "#c084fc",
-    insetShadowPrimary: "#a855f7",
-    insetShadowSecondary: "#e9d5ff",
-  },
-  fuchsia: {
-    background: "#e879f9",
-    insetShadowPrimary: "#d946ef",
-    insetShadowSecondary: "#f5d0fe",
-  },
-  pink: {
-    background: "#f472b6",
-    insetShadowPrimary: "#ec4899",
-    insetShadowSecondary: "#fbcfe8",
-  },
-  rose: {
-    background: "#fb7185",
-    insetShadowPrimary: "#f43f5e",
-    insetShadowSecondary: "#fecdd3",
-  },
-}
+import colors from "tailwindcss/colors"
+
+type Filter = "inherit" | "current" | "transparent" | "black" | "white"
+
+const filteredColors: Omit<typeof colors, Filter> = colors
+
+export const defaultClayColors: Record<string, ClayColor> = Object.fromEntries(
+  Object.entries(filteredColors).map(([colorName, colorValues]) => [
+    colorName,
+    {
+      background: colorValues[400],
+      insetShadowPrimary: colorValues[500],
+      insetShadowSecondary: colorValues[200],
+    },
+  ])
+)
