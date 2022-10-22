1a1c2ea6ac7dc376dd05cf4316d93acf3e6ee567
dulltackle <dulltackle@outlook.com>
2022-10-22 16:53:02 +0800

    refactor: :recycle: refactor default color config

--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,16 +1,114 @@
 import colors from "tailwindcss/colors"
 
-type Filter = "inherit" | "current" | "transparent" | "black" | "white"
-
-const filteredColors: Omit<typeof colors, Filter> = colors
-
-export const defaultClayColors: Record<string, ClayColor> = Object.fromEntries(
-  Object.entries(filteredColors).map(([colorName, colorValues]) => [
-    colorName,
-    {
-      background: colorValues[400],
-      insetShadowPrimary: colorValues[500],
-      insetShadowSecondary: colorValues[200],
-    },
-  ])
-)
+export const defaultClayColors: Record<string, ClayColor> = {
+  slate: {
+    background: colors.slate[400],
+    insetShadowPrimary: colors.slate[500],
+    insetShadowSecondary: colors.slate[200],
+  },
+  gray: {
+    background: colors.gray[400],
+    insetShadowPrimary: colors.gray[500],
+    insetShadowSecondary: colors.gray[200],
+  },
+  zinc: {
+    background: colors.zinc[400],
+    insetShadowPrimary: colors.zinc[500],
+    insetShadowSecondary: colors.zinc[200],
+  },
+  neutral: {
+    background: colors.neutral[400],
+    insetShadowPrimary: colors.neutral[500],
+    insetShadowSecondary: colors.neutral[200],
+  },
+  stone: {
+    background: colors.stone[400],
+    insetShadowPrimary: colors.stone[500],
+    insetShadowSecondary: colors.stone[200],
+  },
+  red: {
+    background: colors.red[400],
+    insetShadowPrimary: colors.red[500],
+    insetShadowSecondary: colors.red[200],
+  },
+  orange: {
+    background: colors.orange[400],
+    insetShadowPrimary: colors.orange[500],
+    insetShadowSecondary: colors.orange[200],
+  },
+  amber: {
+    background: colors.amber[400],
+    insetShadowPrimary: colors.amber[500],
+    insetShadowSecondary: colors.amber[200],
+  },
+  yellow: {
+    background: colors.yellow[400],
+    insetShadowPrimary: colors.yellow[500],
+    insetShadowSecondary: colors.yellow[200],
+  },
+  lime: {
+    background: colors.lime[400],
+    insetShadowPrimary: colors.lime[500],
+    insetShadowSecondary: colors.lime[200],
+  },
+  green: {
+    background: colors.green[400],
+    insetShadowPrimary: colors.green[500],
+    insetShadowSecondary: colors.green[200],
+  },
+  emerald: {
+    background: colors.emerald[400],
+    insetShadowPrimary: colors.emerald[500],
+    insetShadowSecondary: colors.emerald[200],
+  },
+  teal: {
+    background: colors.teal[400],
+    insetShadowPrimary: colors.teal[500],
+    insetShadowSecondary: colors.teal[200],
+  },
+  cyan: {
+    background: colors.cyan[400],
+    insetShadowPrimary: colors.cyan[500],
+    insetShadowSecondary: colors.cyan[200],
+  },
+  sky: {
+    background: colors.sky[400],
+    insetShadowPrimary: colors.sky[500],
+    insetShadowSecondary: colors.sky[200],
+  },
+  blue: {
+    background: colors.blue[400],
+    insetShadowPrimary: colors.blue[500],
+    insetShadowSecondary: colors.blue[200],
+  },
+  indigo: {
+    background: colors.indigo[400],
+    insetShadowPrimary: colors.indigo[500],
+    insetShadowSecondary: colors.indigo[200],
+  },
+  violet: {
+    background: colors.violet[400],
+    insetShadowPrimary: colors.violet[500],
+    insetShadowSecondary: colors.violet[200],
+  },
+  purple: {
+    background: colors.purple[400],
+    insetShadowPrimary: colors.purple[500],
+    insetShadowSecondary: colors.purple[200],
+  },
+  fuchsia: {
+    background: colors.fuchsia[400],
+    insetShadowPrimary: colors.fuchsia[500],
+    insetShadowSecondary: colors.fuchsia[200],
+  },
+  pink: {
+    background: colors.pink[400],
+    insetShadowPrimary: colors.pink[500],
+    insetShadowSecondary: colors.pink[200],
+  },
+  rose: {
+    background: colors.rose[400],
+    insetShadowPrimary: colors.rose[500],
+    insetShadowSecondary: colors.rose[200],
+  },
+}
