2fd7650469f28dfef58d0a6d1a5ba7294b91cc34
dulltackle <dulltackle@outlook.com>
2022-10-26 19:41:16 +0800

    refactor: :recycle: decouple throwing shadow config error to separate module assertClayShadow()

--- src/util/createClayCss.ts
+++ src/util/createClayCss.ts
@@ -1,5 +1,5 @@
-import { isClayShadow } from "./clay-shadow"
 import { assertClayColor } from "./isClayColor"
+import { assertClayShadow } from "./isClayShadow"
 
 interface Props {
   clayColors: unknown
@@ -13,17 +13,14 @@ export const createClayCss = ({ clayColors, clayShadows }: Props) => {
     return Object.entries(clayColors).flatMap(([colorName, colorValue]) => {
       assertClayColor(colorValue)
       return Object.entries(clayShadows).map(([shadowName, shadowValue]) => {
-        if (!isClayShadow(shadowValue)) {
-          throw new Error(`tailwindcss-clay: clay shadow config ${shadowName} is not correct`)
-        } else {
-          return [
-            `clay-${shadowName}-${colorName}`,
-            {
-              backgroundColor: colorValue.background,
-              boxShadow: `${shadowValue.outset},${shadowValue.insetPrimary} ${colorValue.insetShadowPrimary},${shadowValue.insetSecondary} ${colorValue.insetShadowSecondary},${shadowValue.insetModifier}`,
-            },
-          ] as const
-        }
+        assertClayShadow(shadowValue)
+        return [
+          `clay-${shadowName}-${colorName}`,
+          {
+            backgroundColor: colorValue.background,
+            boxShadow: `${shadowValue.outset},${shadowValue.insetPrimary} ${colorValue.insetShadowPrimary},${shadowValue.insetSecondary} ${colorValue.insetShadowSecondary},${shadowValue.insetModifier}`,
+          },
+        ] as const
       })
     })
   }
