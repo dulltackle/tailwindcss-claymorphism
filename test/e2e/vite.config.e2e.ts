0f293b85793cbc90c803b2cce04a9a3231552e75
dulltackle <dulltackle@outlook.com>
2023-05-08 15:24:29 +0800

    chore: :wrench: 整合端到端测试配置

---
+++ test/e2e/vite.config.e2e.ts
@@ -0,0 +1,8 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+// import { resolve } from "path"
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  root: "test/e2e/",
+})
